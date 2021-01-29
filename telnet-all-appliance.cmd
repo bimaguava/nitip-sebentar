9e125bbba260977ada8af3af23d6df38b27f3895
Bima P <45479636+bimsky@users.noreply.github.com>
2021-01-29 22:53:37 +0700

    Create telnet

---
+++ telnet-all-appliance.cmd
@@ -0,0 +1,6 @@
+# cmd /k "%ConEmuDir%\..\init.bat" runAT -new_console:d:"%USERPROFILE%":t:tab2
+# cmd /k "%ConEmuDir%\..\init.bat" runAT -new_console:d:"%USERPROFILE%":n:
+# Reading: https://conemu.github.io/en/NewConsole.html
+
+telnet -new_console:t:ESC_Internet 192.168.137.84 32791 & telnet -new_console:t:ESC_pfSense 192.168.137.84 32785 & telnet -new_console:t:ESC_Router 192.168.137.84 32784
+telnet -new_console:t:EIN_WANemu 192.168.137.84 32773 & telnet -new_console:t:EIN_pfSense 192.168.137.84 32792
