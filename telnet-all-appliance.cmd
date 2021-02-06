cae8b56993b8f710142d35b710c353cb9023761c
Bima P <45479636+bimsky@users.noreply.github.com>
2021-02-06 15:45:04 +0700

    Update telnet-all-appliance.cmd

--- telnet-all-appliance.cmd
+++ telnet-all-appliance.cmd
@@ -2,5 +2,5 @@
 # cmd /k "%ConEmuDir%\..\init.bat" runAT -new_console:d:"%USERPROFILE%":n:
 # Reading: https://conemu.github.io/en/NewConsole.html
 
-telnet -new_console:t:ESC_Internet 192.168.137.84 32791 & telnet -new_console:t:ESC_pfSense 192.168.137.84 32785 & telnet -new_console:t:ESC_Router 192.168.137.84 32784
-telnet -new_console:t:EIN_WANemu 192.168.137.84 32773 & telnet -new_console:t:EIN_pfSense 192.168.137.84 32792
+telnet -new_console:t:ESC_Internet 192.168.137.123 32791 & telnet -new_console:t:ESC_pfSense 192.168.137.123 32785 & telnet -new_console:t:ESC_Router 192.168.137.123 32784
+telnet -new_console:t:EIN_WANemu 192.168.137.123 32773 & telnet -new_console:t:EIN_pfSense 192.168.137.123 32792
